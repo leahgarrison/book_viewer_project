18207ab87afac7b7cd482b10b5cff1657bceb086
leahgarrison <lgarrison1023@gmail.com>
2020-09-04 17:24:39 -0400

    adding project files to repo

---
+++ book_viewer/book_viewer.rb
@@ -0,0 +1,82 @@
+require "tilt/erubis"
+require "sinatra"
+require "sinatra/reloader"
+
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+  @contents = File.readlines("data/toc.txt")
+
+  erb :home
+end
+
+get "/chapters/:number" do 
+  number = params[:number].to_i
+  chapter_name = File.readlines("data/toc.txt")[number-1]
+  @title = "Chapter #{number}: #{chapter_name}"
+
+  @chapter = File.read("data/chp#{number}.txt")
+
+  erb :chapter
+end 
+
+not_found do
+  redirect "/"
+end
+
+get "/search" do
+  @results = chapters_matching(params[:query])
+  erb :search
+end
+
+
+helpers do # formats the chapter into paragraphs
+  def in_paragraphs(chapter)
+     chapter_paragraphs = chapter.split(/\n\n/)
+     chapter_paragraphs.each_with_index.map do |index, text|
+       "<p id=paragraph#{index}>#{text}</p>"
+     end.join   
+  end 
+
+  def highlight(text, term)
+    text.gsub(term, %(<strong>#{term}</strong>))
+  end
+end 
+
+
+
+
+# Calls the block for each chapter, passing that chapter's number, name, and
+# contents.
+def each_chapter
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+# This method returns an Array of Hashes representing chapters that match the
+# specified query. Each Hash contain values for its :name and :number keys.
+def chapters_matching(query)
+  results = []
+
+  return results unless query
+
+  each_chapter do |number, name, contents|
+    matches = {}
+    contents.split("\n\n").each_with_index do |paragraph, index|
+      matches[index] = paragraph if paragraph.include?(query)
+    end
+    results << {number: number, name: name, paragraphs: matches} if matches.any?
+  end
+
+  results
+end
+
+
+
+
